a96c3fa76ef6e0b2928448ce64bb8620467a78b7
Zambrinn <zambrinfilho@gmail.com>
2026-04-21 17:59:48 -0300

    feat: export UsersPage and UnauthorizedPage from index

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,4 @@
 export { LoginPage } from './LoginPage';
 export { RegisterPage } from './RegisterPage';
+export { UsersPage } from './UsersPage';
+export { UnauthorizedPage } from './UnauthorizedPage';
